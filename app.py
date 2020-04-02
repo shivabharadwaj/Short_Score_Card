aea3ac7f3959e8cfb73c80ec7298d5a9bf255c9d
shivabharadwaj <shiva97231@gmail.com>
2020-04-02 12:06:38 -0700

    Update to requirements.txt file.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 
 app.layout = html.Div(children=[
